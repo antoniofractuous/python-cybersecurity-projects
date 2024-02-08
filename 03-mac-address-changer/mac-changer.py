e8b6dd6cbec8c05cbd68c1af8a28129ce30fb64a
Antonio Rojano <82341807+antoniofractuous@users.noreply.github.com>
2024-02-08 11:04:23 -0500

    Update mac-changer.py

--- 03-mac-address-changer/mac-changer.py
+++ 03-mac-address-changer/mac-changer.py
@@ -3,37 +3,39 @@ import subprocess
 import re
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC address to " + interface + " to " + new_mac)
-    subprocess.call(["sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "ifconfig", interface, "ether", new_mac])
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
+    print(f"[+] Changing MAC address: {interface} to {new_mac}")
+    subprocess.call(['sudo', 'ifconfig', interface, 'down'])
+    subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['sudo', 'ifconfig', interface, 'up'])
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change MAC address")
-    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address for interface")
+    parser.add_argument("-i", dest="interface", help="Interface to change MAC address")
+    parser.add_argument("-m", dest="new_mac", help="MAC address value to change")
     options = parser.parse_args()
     if not options.interface:
-        print("[-] Please specify an interface, type --help for more info")
+        print("[-] Please specify an interface, type --help, for more info")
     elif not options.new_mac:
         print("[-] Please specify a MAC address, type --help for more info")
     return options
 
 def get_mac(mac):
-    ifconfig_result = subprocess.check_output(["ifconfig", mac])
-    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
-    if mac_result:
-        return mac_result.group(0)
+    ifconfig_output = subprocess.check_output(['ifconfig', mac])
+    check_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_output))
+    if check_mac:
+        return check_mac.group(0)
     else:
-        print("[-] Could not read MAC address, type --help for more info")
-
+        print("[-] Could not retrieve MAC address, type --help for more info")
+    
 options = get_arguments()
-mac_retrieve = get_mac(options.interface)
-print("Current MAC = " + str(mac_retrieve))
+mac_regex = get_mac(options.interface)
+print("Current MAC address = ", str(mac_regex))
 
 change_mac(options.interface, options.new_mac)
 
-if mac_retrieve == options.new_mac:
-    print("[+] MAC address was successfully changed to " + mac_retrieve)
+new_mac_regex = get_mac(options.interface)
+
+if new_mac_regex == options.new_mac:
+    print(f"[+] MAC address successfully changed to {mac_regex}")
 else:
-    print("[-] MAC address could not be changed, use --help for more info. ")
+    print("[-] Could not change MAC address, type --help for more info")
